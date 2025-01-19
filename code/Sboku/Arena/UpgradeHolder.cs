2cbad91a1f23f8825fca325ad3f45a5db7eb16b2
Konstantin <49278699+KonstantinRight@users.noreply.github.com>
2025-01-19 03:15:46 +0300

    Gameplay adjustments

--- code/Sboku/Arena/UpgradeHolder.cs
+++ code/Sboku/Arena/UpgradeHolder.cs
@@ -142,7 +142,7 @@ public class UpgradeHolder : Component
     }
     public float DamageMultiplier { get => DamageClass switch
         {
-            Level.A => 10f,
+            Level.A => 8f,
             Level.B => 5f,
             Level.C => 3f,
             Level.D => 1.5f,
@@ -153,8 +153,8 @@ public class UpgradeHolder : Component
     }
     public float ArmorMultiplier { get => ArmorClass switch
         {
-            Level.A => 0.05f,
-            Level.B => 0.11f,
+            Level.A => 0.1f,
+            Level.B => 0.15f,
             Level.C => 0.25f,
             Level.D => 0.5f,
             Level.E => 0.75f,
